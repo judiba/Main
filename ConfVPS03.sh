fd235da61414b3b29712d5a5315192a1d96fd6a5
judiba <judioba@gmail.com>
2017-05-18 16:22:05 -0300

    Update ConfVPS03.sh

--- ConfVPS03.sh
+++ ConfVPS03.sh
@@ -24,13 +24,13 @@ fi
 if [ "$s" = "s" ]
 then
 clear
-echo " \033[46;1;37m Instalando Dropbear aguarde \033[0m"
+echo -e "\033[46;1;37m Instalando Dropbear aguarde \033[0m"
 sleep 3
 apt-get install dropbear -y
 printf '#Dropbear By: judiba\n NO_START=0\nDROPBEAR_PORT=443\nDROPBEAR_EXTRA_ARGS="-p 109 -p110\nDROPBEAR_BANNER="/etc/bannervps"\nDROPBEAR_RECEIVE_WINDOW=65536' > /etc/default/dropbear
 service dropbear restart 
 sleep 3
 clear
-echo " \033[46;1;37m Dropbear configurado com sucesso nas portas 109 e 110 porém trabalhando na porta 443 via SSLH !!!\033[0m"
+echo -e "\033[46;1;37m Dropbear configurado com sucesso nas portas 109 e 110 porém trabalhando na porta 443 via SSLH !!!\033[0m"
 fi
 exit
